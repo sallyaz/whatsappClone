fa1187d96c067fcb4fb38f39908fed4af8ed28c2
Sally az <sallyazulay@gmail.com>
2023-06-06 00:10:45 +0300

    added form validation logic

--- utils/formHandler.js
+++ utils/formHandler.js
@@ -3,17 +3,27 @@ import {validate} from 'validate.js';
 export const formHandler = (inputId, inputValue) => {
   const consntraints = {
     presence: {allowEmpty: false},
-    format: {
+  };
+
+  if (inputId == 'First Name' || inputId == 'Last Name') {
+    consntraints.format = {
       pattern: '[a-z]+',
       flags: 'i',
       message: 'can only contain letters',
-    },
-    email: true,
-  };
+    };
+  } else if (inputId == 'Email') {
+    consntraints.email = true;
+  } else if (inputId == 'Password') {
+    consntraints.length = {
+      minimum: 6,
+      message: 'Password must be more than 6 charecters',
+    };
+  }
 
   const validationResult = validate(
     {[inputId]: inputValue},
     {[inputId]: consntraints},
   );
+
   return validationResult && validationResult[inputId];
 };
